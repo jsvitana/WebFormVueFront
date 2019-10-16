6a4d2ab96c5fa5b1da325fc5a47da8678a1032aa
Jonathan <jsvitana@yahoo.com>
2019-10-16 08:55:57 -0400

    Basic Vue working

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 var app = new Vue({
     el: '#test',
+    data: {
+        message: ''
+    },
+    created () {
+        fetch('https://api.myjson.com/bins/17mt8m')
+        .then((resp) => resp.json()
+        .then((data) => {
+            this.message = data.emails[1]
+            }))
+    }
 })
+
  
